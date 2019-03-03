ec605378bfa961880b52f3ba8f65f573d7a7fa9d
sandesh <shettysandesh1918@gmail.com>
2019-03-03 15:23:08 +0530

    div method

--- math.c
+++ math.c
@@ -4,10 +4,16 @@ void add(float, float)
 {
 printf("add function");
 }
+void div(f1,f2)
+{
+printf("division function");
+}
+
 void main()
 {
 printf("hello world");
 printf("my name");
 //hello world program
 add(float,float);
+div(f1,f2);
 }
